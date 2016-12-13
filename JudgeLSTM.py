d4c4327857d52c62922bb782da19ecbc4e9f4389
HydroIT <hydroit@gmail.com>
2016-12-13 17:02:01 +0200

    JudgeLSTM, Fixed MidiLSTM

---
+++ JudgeLSTM.py
@@ -0,0 +1,194 @@
+import random
+import music21
+import keras
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+import numpy as np
+from MidiLSTM import MidiLSTM
+
+"""
+An attempt at neural-network-based evaluation function for midi files.
+Final result gives a probability for each "genre" from the following list:
+0	corpus.getComposer('bach')
+1	corpus.getComposer('beethoven')
+2	corpus.getComposer('essenFolksong')
+3	corpus.getComposer('monteverdi')
+4	corpus.getComposer('oneills1850')
+5	corpus.getComposer('palestrina')
+6	corpus.getComposer('ryansMammoth')
+7	corpus.getComposer('trecento')
+The probabilities can then be used as evaluating all the different aspects of creativity:
+-value: Does this NN consider the midi stream to be > 0.5 for any "genre"?
+-novelty: How far is the score given by the NN to the agent's idea of his genre? (i.e. the loss function?)
+-surprisingness: Incorporating the agents data, if he hasn't seen anything from genre 4 but got the highest score
+there, then quite surprising? etc...
+
+This network does not take into account the length\duration, but can be extended to such quite easily.
+"""
+
+class JudgeLSTM:
+    """
+    A Judge LSTM network - learns and predicts a genre from the above composers.
+    A shallow LSTM network (LSTM -> Dropout -> Fully Connected Layer (softmax))
+    """
+    ELEMENT_LENGTH = 3  # each element in X (before encoding) has this many elements (i.e. |x_i| = 3))
+    EOS = tuple([-1] * ELEMENT_LENGTH)  # End of Song symbol
+    rest = 0  # Rest symbol
+    n_composers = len(MidiLSTM.composers.keys())  # Total composers to evaluate from
+
+    def __init__(self, timesteps=768, hidden_dim=256, data_dim=ELEMENT_LENGTH, batch_size=16):
+        """
+        Initializes the LSTM Judge agent.
+        :param timesteps: How long is each sequence (sequences shorter than this will be padded to match this length,
+        sequences longer than this number will be truncated)
+        :param hidden_dim: The hidden dimension for the LSTM layer
+        :param data_dim: The data given at each time step
+        :param batch_size: How many samples are required to update while training
+        """
+        self.timesteps = timesteps
+        self.data_dim = data_dim
+        self.batch_size = batch_size
+
+        # Set up some checkpoint (for training)
+        filepath = "weights-classify-{epoch:02d}-{loss:.4f}.hdf5"
+        checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+        self.callbacks_list = [checkpoint]
+
+        # Model creation (sequential model)
+        self.model = keras.models.Sequential()
+        # LSTM layer, outputs #timesteps vectors of dimension 128
+        self.model.add(keras.layers.LSTM(output_dim=hidden_dim, input_shape=(timesteps, data_dim)))
+        # Dropout layer
+        self.model.add(keras.layers.Dropout(0.1))
+        # Last layer - reduce to 8-dimension vector and softmax over it to get probability
+        self.model.add(keras.layers.Dense(JudgeLSTM.n_composers, activation='softmax'))
+        # Compile with categorical cross-entropy loss and adadelta optimizer
+        self.model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+    def load_weights(self, filename):
+        """
+        Loads the weights from the HDF5 file to the LSTM model
+        """
+        self.model.load_weights(filename)
+
+    def train(self, epochs=100, n=20):
+        """
+        Trains the entire network on creations embedded in music21
+        :param epochs: How many epochs to run
+        :param n: How many samples from each genre
+        """
+        x_data = list()
+        y_data = list()
+        for i in MidiLSTM.composers.keys():
+            x, y = self.get_data(i, n)  # Get the data for composer #i
+            x_data += x
+            y_data += y
+        # Fit the model
+        self.model.fit(np.array(x_data), np_utils.to_categorical(y_data, nb_classes=JudgeLSTM.n_composers),
+                       nb_epoch=epochs, batch_size=self.batch_size, callbacks=self.callbacks_list)
+
+    def fit_single(self, data, label, iterations):
+        """
+        Fits a single sample (data) and label (mainly for interaction with a multi agent system)
+        :param data: A music21 stream, from which the notes and durations will be extracted
+        :param label: The matching composer
+        :param iterations: How many "epochs" on this specific example (default 2)
+        """
+        # Convert data to matching types
+        x = np.array([JudgeLSTM._stream2ints(data, self.timesteps)])
+        y = np_utils.to_categorical([label], nb_classes=JudgeLSTM.n_composers)
+        self.model.fit(x, y, nb_epoch=iterations, verbose=0)
+
+    def predict(self, sequence):
+        """
+        Predicts a probability over the different composers, given the music21 stream
+        :param sequence: music21 stream from which notes will be extracted (and padded\truncated to matching length)
+        :return: A distribution over the composers (as a list)
+        """
+        x = np.array([JudgeLSTM._stream2ints(sequence, self.timesteps)])
+        return self.model.predict(x).tolist()[0]  # Returns a 2d matrix with 1 row...
+
+    @staticmethod
+    def _midi_values(m21obj):
+        """
+        Takes a music21 object (note\chord\rest) and returns an ELEMENT_LENGTH-tuple, where
+        each element is the midi value for the corresponding note. Chords are broken into the tuple appropriately.
+        Rests are denoted by 0, and invalid values are -1.
+        This method is quite naive but it is only for preprocessing, so we don't mind :)
+        """
+        result = list()
+        if m21obj.isNote:  # Just a note, add the midi value
+            result.append(m21obj.pitch.midi)
+        elif m21obj.isChord:
+            for p in m21obj.pitches:  # Add all the midi values from the chord, truncating excess values
+                if len(result) < JudgeLSTM.ELEMENT_LENGTH:
+                    result.append(p.midi)
+                else:
+                    break
+        elif m21obj.isRest:  # Rest
+            result.append(JudgeLSTM.rest)
+        else:  # Invalid.
+            result = JudgeLSTM.EOS
+        while len(result) < JudgeLSTM.ELEMENT_LENGTH:  # Append more "rests"
+            result.append(JudgeLSTM.rest)
+        return tuple(result)
+
+    @staticmethod
+    def _pad_sequence(seq, maxlen):
+        """
+        Pads the given sequence (seq) to have exactly maxlen items, and adds the EOS symbol as the last one.
+        If there are more than maxlen elements in the sequence, it is truncated
+        (only the first maxlen symbol are considered)
+        This method is also quite naive, but again - preprocessing....
+        """
+        if len(seq) >= maxlen:
+            seq = seq[:maxlen-1]  # truncate
+        seq.append(JudgeLSTM.EOS)  # Append EOS
+        t = tuple([JudgeLSTM.rest] * JudgeLSTM.ELEMENT_LENGTH)  # Filler
+        while len(seq) < maxlen:
+            seq.append(t)
+        return seq
+
+    @staticmethod
+    def _stream2ints(seq, maxlen):
+        """
+        Converts a given music21 stream to a padded list of ints
+        :param seq: music21 stream from which notes and durations will be extracted
+        :param maxlen: How long should the output be
+        :return: A padded list representing the sequence
+        """
+        inputs = list(seq.sorted.flat.getElementsByClass(["Note", "Chord", "Rest"]))
+        return JudgeLSTM._pad_sequence([JudgeLSTM._midi_values(x) for x in inputs], maxlen)
+
+    def get_data(self, composer_index, n=20):
+        """
+        Gets n samples from the composer index in padded int-sequence format
+        :param composer_index: Matches one of the composers in MidiLSTM.py
+        :param n: Number of samples from composer (None for all files, default is 20)
+        :return: A tuple (X, Y) for the matching data
+        """
+        X = list()
+        Y = list()
+        # Get file list
+        files = music21.corpus.getComposer(MidiLSTM.composers[composer_index])
+        if n is not None:
+            files = random.sample(files, n)  # Sample as required
+        for f in files:
+            try:
+                chorale_stream = music21.corpus.parse(f)  # Parse the file to stream
+                X.append(JudgeLSTM._stream2ints(chorale_stream, self.timesteps))  # Convert and pad
+                Y.append(composer_index)  # Add matching label
+            except:  # Skip errorneous files
+                continue
+        return X, Y
+
+
+# Uncomment for a free test :)
+test = JudgeLSTM()
+test.train(epochs=100, n=20)
+# test.load_weights("weights-classify-70-0.3340.hdf5")
+# print(test.predict(music21.corpus.parse(music21.corpus.getComposer('beethoven')[8])))
+# test.fit_single(music21.corpus.parse(music21.corpus.getComposer('bach')[0]), 0, 2)
+# print(test.predict(music21.corpus.parse(music21.corpus.getComposer('bach')[0])))
+
+#EOF
--- MidiLSTM.py
+++ MidiLSTM.py
@@ -6,6 +6,7 @@ from keras.callbacks import ModelCheckpoint
 import numpy as np
 import random
 
+
 class MidiLSTM:
     """
     An attempt at using deep LSTM network to learn from midi files and generates music thereafter.
@@ -14,35 +15,44 @@ class MidiLSTM:
     EOS = -1  # End of Song symbol
     rest = 0  # Rest symbol
     last_note = 129  # last note midi value (+1 so rest is 0)
+    # Composers dictionary (for comparison, generation, etc)
     composers = {0: 'bach', 1: 'beethoven', 2: 'essenFolksong', 3: 'monteverdi',
                  4: 'oneills1850', 5: 'palestrina', 6: 'ryansMammoth', 7: 'trecento'}
     vocabulary_size = 129  # 128 midi values + rest (originally 82 if we want to use previous weights)
     input_dim = 1  # Only one note per time step
 
     def __init__(self, composer_index, timesteps=64, hidden_dim=256, batch_size=128):
+        """
+        Initializes a new agent that produces single notes based on LSTM implementation
+        and adds duration based on Markov Chain model.
+        :param composer_index: Composer index to associate with this agent
+        :param timesteps: How many timesteps are in the LSTM model (default 64)
+        :param hidden_dim: How big is the hidden dimension for the LSTM model (default 256)
+        :param batch_size: How many samples are required for an update (default 128)
+        """
         if composer_index in MidiLSTM.composers.keys():
             self.composer_idx = composer_index
-            self.hidden_dim = hidden_dim
-            self.timesteps = timesteps
-            self.batch_size = batch_size
+            self.hidden_dim = hidden_dim  # Hidden dimensionality between time steps
+            self.timesteps = timesteps  # How many time steps are used for prediction
+            self.batch_size = batch_size  # How many samples are required for update
             # Create the model (Sequential model)
             self.model = Sequential()
             # First LSTM layer, returns a matching sequence of vectors of dimension hidden_dim
-            self.model.add(LSTM(self.hidden_dim, input_shape=(self.timesteps, MidiLSTM.input_dim),
-                                return_sequences=True))
+            self.model.add(LSTM(self.hidden_dim, input_shape=(self.timesteps, MidiLSTM.input_dim)))#,
+                                #return_sequences=True))
             # First Dropout layer (forget some samples, prevents overfitting)
             self.model.add(Dropout(0.15))
             # Second LSTM layer, returns a matching sequence of vectors of dimension hidden_dim
-            self.model.add(LSTM(self.hidden_dim, return_sequences=True))
+            #self.model.add(LSTM(self.hidden_dim, return_sequences=True))
             # Second Dropout layer (forget some samples, prevents overfitting)
-            self.model.add(Dropout(0.1))
+            #self.model.add(Dropout(0.1))
             # Third LSTM layer, returns one final output vector of hidden_dim (representing the entire sequence)
-            self.model.add(LSTM(self.hidden_dim))
+            #self.model.add(LSTM(self.hidden_dim))
             # Final layer, reduce to tag-one-hot-encoding and softmax over it
             self.model.add(Dense(MidiLSTM.vocabulary_size, activation='softmax'))
             # Compile with categorical cross-entropy loss and ada-delta optimizer
-            self.model.compile(loss='categorical_crossentropy', optimizer='adadelta')
-            # Checkpoint (for loading weights after training)
+            self.model.compile(loss='categorical_crossentropy', optimizer='adam')
+            # Checkpoint (for loading\saving weights after training)
             filepath = "weights-composer-" + str(composer_index) + "-{epoch:02d}-{loss:.4f}.hdf5"
             self.callbacks_list = [ModelCheckpoint(filepath, monitor='loss', verbose=1,
                                                    save_best_only=True, mode='min')]
@@ -73,14 +83,14 @@ class MidiLSTM:
             sequence_length = self.timesteps
         pattern = np.random.randint(MidiLSTM.rest, MidiLSTM.last_note, size=self.timesteps).tolist()
         result = pattern
-        for i in range(iterations):
+        for _ in range(iterations):
             x = np.array(pattern, dtype='float32')
             x = np.reshape(x, (1, self.timesteps, MidiLSTM.input_dim))  # One sample
             prediction = np.argmax(self.model.predict(x, verbose=0))
             pattern.append(prediction)
-            pattern = pattern[1:self.timesteps]
+            pattern = pattern[1:]
             result.append(prediction)
-            if result > sequence_length:
+            if len(result) > sequence_length:
                 result = result[1:sequence_length]
         return result
 
@@ -102,10 +112,13 @@ class MidiLSTM:
                 files = random.sample(files, n)
             data_raw = list()
             for f in files:
-                mstream = music21.corpus.parse(f)
-                inputs = list(mstream.sorted.flat.getElementsByClass(["Note", "Chord", "Rest"]))
-                inputs_midi = [MidiLSTM._to_midi_values(x) for x in inputs]
-                data_raw.append(inputs_midi)
+                try:
+                    mstream = music21.corpus.parse(f)
+                    inputs = list(mstream.sorted.flat.getElementsByClass(["Note", "Chord", "Rest"]))
+                    inputs_midi = [MidiLSTM._to_midi_values(x) for x in inputs]
+                    data_raw.append(inputs_midi)
+                except:
+                    continue
             return data_raw, len(data_raw)
         else:
             raise Exception("Composer index not found")
@@ -122,8 +135,12 @@ class MidiLSTM:
         return stream
 
 
-# EOF
+# # Train for now...
+# bach = MidiLSTM(0)
+# bach.load_weights("weights-composer-0-49-2.8121.hdf5")
+# bach.train(epochs=50, n=20)
+# result = bach.generate()
+# print(result)
+# MidiLSTM._to_midi_stream(result)
 
-# Train for now...
-bach = MidiLSTM(0)
-bach.train(epochs=50, n=20)
+# EOF
